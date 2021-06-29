eb37559f6557faa17e63399b848d38c203e5c98c
Marcin Czekajski <czekajskimarcin@gmail.com>
2021-06-29 20:01:17 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+const users = [{ name: "Name" }];
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/users", (req, res) => {
+  res.json(users);
+  console.log("Get users req");
+});
+
+app.post("/users", async (req, res) => {
+  try {
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    console.log(salt);
+    console.log(hashedPassword);
+    const user = { name: req.body.name, password: hashedPassword };
+    users.push(user);
+    res.status(201).send();
+  } catch {
+    res.status(500).send();
+  }
+});
+
+app.post("/users/login", async (req, res) => {
+  const user = users.find((user) => (user.name == req.body.name));
+  if (user == null) {
+    return res.status(400).send("Cannot find user");
+  }
+  try {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      res.send("Success");
+    } else {
+      res.send("Not allowed");
+    }
+  } catch {
+    res.status(500).send();
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
