59a38561360b57e213a55d27c4293fd9460e364a
Marcin Czekajski <czekajskimarcin@gmail.com>
2021-07-01 20:38:07 +0200

    heroku try

--- index.js
+++ index.js
@@ -6,9 +6,8 @@ const mongoose = require('mongoose')
 const bcrypt = require("bcrypt");
 const port = process.env.PORT || 80;
 
-let db;
-
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(db = mongoose.connection);
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection;
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
 
